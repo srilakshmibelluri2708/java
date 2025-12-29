1ad1a9c34c964ccb9312009e181478e2e7bc4596
srilakshmibelluri2708 <srilakshmibelluri2708@gmail.com>
2025-12-29 20:38:11 +0530

    6 table

---
+++ tns/src/com/tnsif/intro/task2/table6.java
@@ -0,0 +1,10 @@
+package com.tnsif.intro.task2;
+
+public class table6 {
+	public static void main(String args[]) {
+		int n=6;
+		for(int i=1;i<=10;i++){
+			System.out.println(n+"x"+i+"="+(n*i));
+		}
+	}
+}
